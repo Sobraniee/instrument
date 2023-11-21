9bfba9c61ac63d7330e211d9c5ce06cab6b51067
JohnnyEvs <sj1807@mail.ru>
2023-11-21 17:52:06 +0900

    brand api view

--- core/views/brand.py
+++ core/views/brand.py
@@ -1,3 +1,5 @@
+from rest_framework.views import APIView
+from rest_framework.response import Response
 from django.shortcuts import render
 from django.views import View
 from core.models import Brand
@@ -6,4 +8,18 @@ from core.models import Brand
 class BrandView(View):
     def get(self, request, *args, **kwargs):
         brand = Brand.objects.all()
-        return render(request, {'brand': brand})
+        return render(request, {"brand": brand})
+
+
+class BrandListAPIView(APIView):
+    def get(self, request):
+        brands = Brand.objects.all()
+        serializer = Brand(brands, many=True)
+        return Response(serializer.data)
+
+
+class BrandDetailAPIView(APIView):
+    def get(self, request, pk):
+        brand = Brand.objects.get(pk=pk)
+        serializer = Brand(brand)
+        return Response(serializer.data)
