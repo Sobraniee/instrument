2121ff70830dfa1d1917c3229d7b819b28e6e8e1
Sobraniee <141002334+Sobraniee@users.noreply.github.com>
2023-11-27 02:44:45 -0700

    ca

--- core/views/category.py
+++ core/views/category.py
@@ -1,6 +1,6 @@
 from rest_framework import generics
 from core.models import Category
-from core.serializers.category import CategorySerializer
+from core.serializers import CategorySerializer
 
 
 class CategoryListCreateView(generics.ListCreateAPIView):
