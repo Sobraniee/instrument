d3ec149cb572127274eb8724135c82b6502d6361
JohnnyEvs <sj1807@mail.ru>
2023-11-28 16:46:13 +0900

    minor bugs fixed

--- core/models/tools.py
+++ core/models/tools.py
@@ -1,19 +1,18 @@
 from django.db import models
 from .brand import Brand
 from .category import Category
-from .subcategory import Subcategory
-
 
 class Tools(models.Model):
     name = models.CharField(max_length=255)
     description = models.TextField()
     type = models.CharField(max_length=255)
     price = models.DecimalField(max_digits=10, decimal_places=2)
-    category = models.ForeignKey("Category", on_delete=models.CASCADE)
-    brand = models.ForeignKey('Brand', on_delete=models.CASCADE)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    brand = models.ForeignKey(Brand, on_delete=models.CASCADE)
     photos = models.ImageField(upload_to='photos')
     favorites = models.BooleanField(default=False)
 
     class Meta:
         verbose_name = "Инструмент"
         verbose_name_plural = "Инструменты"
+
