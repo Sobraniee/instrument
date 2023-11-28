15a71ef31392ca70a22ef0df02b88872903e48ad
Sobraniee <141002334+Sobraniee@users.noreply.github.com>
2023-11-28 02:07:48 -0700

    Revert "subcategory"

--- core/models/category.py
+++ core/models/category.py
@@ -3,8 +3,7 @@ from django.db import models
 
 class Category(models.Model):
     name = models.CharField(max_length=255)
-    subcategory = models.ForeignKey('Subcategory', on_delete=models.CASCADE)
-    
+
     class Meta:
         verbose_name = 'Категория'
         verbose_name_plural = 'Категории'
